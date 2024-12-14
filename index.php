e1ff6a876839a6ce15ec57789136704a13d58b2b
onono20 <onono2077@gmail.com>
2024-12-14 23:21:43 +0900

    ツール枠組み

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php echo 'Hello World !' ?>
+    <div class="main">
+        <div class="content-jira">
+            <span>JIRA</span>
+            <div class="container">
+                <textarea name="text_jira" id=""></textarea>
+            </div>
+        </div>
+        <div class="content-pr">
+            <span>git PR</span>
+            <div class="container">
+                <textarea name="text_pr" id=""></textarea>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
